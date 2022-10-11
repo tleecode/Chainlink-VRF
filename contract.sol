86635b503de9951c61be54be0f6798b66f774c34
tleecode <104124461+tleecode@users.noreply.github.com>
2022-10-11 12:12:55 -0400

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -39,7 +39,7 @@ contract ZombieFactory is VRFConsumerbase {
         randomResult = randomness;
     }
 
-    // Delete the function below
+  
     function _generatePseudoRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
